4b82e457b0a21a1d18ee344f1f261dbc3a057c81
jin-us <leejinus@gmail.com>
2019-10-23 10:28:12 +0900

    Add page

--- api-python/api-call2.py
+++ api-python/api-call2.py
@@ -1,49 +1,36 @@
 import requests
 from urllib.parse import urlparse
-page = 0
-# keyword = input("검색 내용 : ")
+import sys
+index = 0
+page = 1
+keyword = input("검색 내용 : ")
+
+def search(index):
+    temp = str(page)
+    url = "https://api.github.com/search/repositories?q=" + keyword +"&sort=stars&order=desc&page=" + temp
+    result = requests.get(urlparse(url).geturl())
+    json_obj = result.json()
+
+    num = int(json_obj["total_count"])
+    print(num, "개 검색", sep='')
+
+    for i in range(0, index+30):
+        try:
+            print(i+1+(page*30-30),"번째")
+            print (json_obj["items"][i]["owner"]["login"])
+            print(json_obj["items"][i]["html_url"])
+            print (json_obj["items"][i]["stargazers_count"], "\n")
+        except:
+            print("")
 
 
-url = "https://api.github.com/search/repositories?q="+ keyword +"&sort=stars&order=desc" +page
-result = requests.get(urlparse(url).geturl())
-json_obj = result.json()
-
-num = int(json_obj["total_count"])
-print(num, "개 검색", sep='')
-# print(len(json_obj)
-
-
-for i in range(0, num):
-    try:
-        print(i+1,"번째")
-        print (json_obj["items"][i]["owner"]["login"])
-        print(json_obj["items"][i]["html_url"])
-        print (json_obj["items"][i]["stargazers_count"], "\n")
-    except:
-        print("")
-
-
-    # if json_obj["items"][i]["owner"]["login"] != []:
-    #     print(json_obj["items"][0]["owner"]["login"])
-    #     print(json_obj["items"][0]["owner"]["html_url"] + "\n")
-    # else:
-    #     json_obj["items"][0]["owner"]["html_url"] = ""
-
-
-
-
-
-
-
-
-
-# print (json_obj["items"][0]["owner"])
-#print(json_obj)
-
-# del json_obj['items']
-
-# print(json_obj)
-
-# print(json_obj['items'])
-#print(json_obj)
-
+while(1):
+    search(index)
+    print("Next 입력시 다음 페이지")
+    print(page, "Page")
+    A = sys.stdin.readline().split()
+    if(A == "Next") | (index == 0):
+        index = index
+        page = page+1
+    else:
+        break
